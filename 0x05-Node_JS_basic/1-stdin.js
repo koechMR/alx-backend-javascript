b50e4c376984fe92328301c308431a4dcb032e5a
koechMR <briankipkoech328@gmail.com>
2024-02-26 10:38:56 +0300

    using process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    console.log('This important software is now closing');
+    rl.close();
+  } else {
+    console.log(`Your name is: ${input}`);
+  }
+});
